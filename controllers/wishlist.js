d1b402ea8e0ef2b0c77ada685132516990478f8c
shivakokkula <kokkulashivakumar1@gmail.com>
2024-02-14 10:53:41 +0530

    commit

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -1,30 +1,15 @@
 import { prisma } from "../initializer/initprisma.js";
 import crud from "../utils/crud.js"
-import { sendresponse } from "../utils/utils.js";
 
 let include = { product: true };
 
 const get = async (req, res, next) => crud.get(req, res, next, prisma.wishlist, include, { user_id: Number(req.user.id) });
-const getbyid = async (req, res, next) => crud.getbyid(req, res, next, prisma.wishlist, include);
 const deletebyid = async (req, res, next) => crud.deletebyid(req, res, next, prisma.wishlist, include);
-const put = async (req, res, next) => crud.update(req, res, next, prisma.wishlist, required, include);
-
 const post = async (req, res, next) => {
-    try {
-        let { wishlist } = req.body;
-        const user_id = Number(req.user.id);
-        const created_on = new Date();
-        for (let i = 0; i < wishlist.length; i++) {
-            wishlist[i].user_id = user_id;
-            wishlist[i].created_on = created_on;
-        }
-        req.body.user_id = user_id;
-        await prisma.wishlist.deleteMany({ where: { user_id } });
-        const data = await prisma.wishlist.createMany({ data: wishlist });
-        sendresponse(res, data, 201, req);
-    } catch (e) {
-        next(e);
-    }
+    const { product_id } = req.body;
+    const required = { product_id };
+    req.body.user_id = Number(req.user.id);
+    await crud.create(req, res, next, prisma.wishlist, required, include,{product_id,user_id: req.body.user_id});
 }
 
-export default { get, getbyid, post, put, deletebyid };
+export default { get, post, deletebyid };
--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,6 @@ import express from "express";
 const router = express.Router();
 router
   .get('/', controller.get)
-  .get('/:id', controller.getbyid)
   .post("/", controller.post)
   .put("/:id", controller.put)
   .delete("/:id", controller.deletebyid)
